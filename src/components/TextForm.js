2809b263c1e9ba40673ab473db2bf2753578e81f
jaikishan223 <105590520+jaikishan223@users.noreply.github.com>
2022-08-07 14:49:21 +0530

    This app is about performing many operations on the text you have inputted

---
+++ src/components/TextForm.js
@@ -0,0 +1,74 @@
+import React, { useState } from 'react'
+
+
+export default function TextForm(props) {
+    const handleUpClick = () => {
+        // console.log("Uppercase was clicked" + text);
+        let newText = text.toUpperCase();
+        setText(newText)
+        props.showAlert("Converted to UpperCase","success")
+    }
+
+
+    const handleLoClick = () => {
+        // console.log("Uppercase was clicked" + text);
+        let newText = text.toLowerCase();
+        setText(newText)
+        props.showAlert("Converted to LowerCase","success")
+    }
+
+    const handleClearClick = () => {
+        let newText = "";
+        setText(newText)
+        props.showAlert("Text cleared","success")
+    }
+
+    const handleOnChange = (event) => {
+        // console.log("On change");
+        setText(event.target.value);
+        
+    }
+
+    const handleCopy = () => {
+        // console.log('I am Copy');
+        let text = document.getElementById('mybox');
+        text.select();
+        props.showAlert("Copied to clipboard","success")
+
+        // text.selectionRange(0,9999);
+        navigator.clipboard.writeText(text.value);
+    }
+
+    const handleExtraSpaces = () => {
+        let newText = text.split(/[ ]+/);
+        setText(newText.join(" "))
+        props.showAlert("Removed extra spaces","success")
+    }
+
+    const [text, setText] = useState('');  //Enter text here
+    // text ="new text"; (Wrong way to change the text plz use appropiate function)
+    //setText("new text"); (Correct way to change the state)
+
+    return (
+        <>
+            <div className='container' style={{color:props.mode==='dark'?'white':'#042743'}}>
+                <h1 >{props.heading}</h1>
+                <div className="mb-3">
+                    <textarea className="form-control" value={text} onChange={handleOnChange} style={{backgroundColor:props.mode==='dark'?'grey':'white',color:props.mode==='dark'?'white':'#042743'}} id="mybox" rows="8"></textarea>
+                </div>
+                <button className="btn btn-primary mx-2" onClick={handleUpClick}>Convert to UpperCase</button>
+                <button className="btn btn-primary mx-2" onClick={handleLoClick}>Convert to LowerCase</button>
+                <button className="btn btn-primary mx-2" onClick={handleClearClick}>Clear</button>
+                <button className="btn btn-primary mx-2" onClick={handleCopy}>Copy Text</button>
+                <button className="btn btn-primary mx-2" onClick={handleExtraSpaces}>Remove Extra Spaces</button>
+            </div>
+            <div className="container my-2" style={{color:props.mode==='dark'?'white':'#042743'}}>
+                <h2>Your text summary</h2>
+                <p>{text.split("").length} words and {text.length} characters</p>
+                <p>{0.008 * text.split("").length}Minutes read</p>
+                <h2>Preview</h2>
+                <p>{text.length>0?text:"Enter something topreview it here"}</p>
+            </div>
+        </>
+    )
+}
